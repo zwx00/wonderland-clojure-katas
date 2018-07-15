4ad099db37c9ad2660e3b447cb3a93825a410ba9
zwx00 <bblokar@gmail.com>
2018-07-15 02:11:33 +0200

    encode...

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,31 @@
 (ns alphabet-cipher.coder)
 
+(use '[clojure.string :only [index-of, join]])
+
+(def alphabet (map char (range 97 123)))
+
+
+(defn alphabet-index [l] (index-of (join alphabet) l))
+
+(defn match-letters [a, b]
+  (nth
+    (cycle alphabet)
+      (+
+        (alphabet-index a)
+        (alphabet-index b)
+      )
+  )
+)
+
 (defn encode [keyword message]
-  "encodeme")
+  (join
+    (map match-letters
+       message (cycle keyword)
+
+    )
+  )
+)
+
 
 (defn decode [keyword message]
   "decodeme")
