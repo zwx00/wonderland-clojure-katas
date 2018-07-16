86b1e26794fb9a2069e2c793e980d0b4ab14fd51
zwx00 <bblokar@gmail.com>
2018-07-16 18:56:42 +0200

    working solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,35 +1,55 @@
 (ns alphabet-cipher.coder)
 
 (use '[clojure.string :only [index-of, join]])
+(use '[clojure.data :only [diff]])
 
 (def alphabet (map char (range 97 123)))
-
 
 (defn alphabet-index [l] (index-of (join alphabet) l))
 
 (defn match-letters [a, b]
   (nth
-    (cycle alphabet)
-      (+
-        (alphabet-index a)
-        (alphabet-index b)
-      )
-  )
-)
+   (cycle alphabet)
+   (+
+    (alphabet-index a)
+    (alphabet-index b))))
+
+(defn unmatch-letters [a, b]
+  (nth
+   (cycle alphabet)
+   (+ 26
+      (-
+       (alphabet-index a)
+       (alphabet-index b)))))
+
+(defn extract-keyword [a, b]
+  (nth
+   (cycle alphabet)
+   (+ 26
+      (-
+       (alphabet-index b)
+       (alphabet-index a)))))
 
 (defn encode [keyword message]
   (join
-    (map match-letters
-       message (cycle keyword)
-
-    )
-  )
-)
-
+   (map match-letters
+        message (cycle keyword))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (join
+   (map unmatch-letters
+        message (cycle keyword))))
+
+(defn find-pattern
+  [letters]
+  (some #(when % %)
+  (for [c (range (count letters)) :when (> c 1)]
+               (let [partitions (partition-all c letters)]
+                 (if (apply = (butlast partitions))
+                   (if (= (last (diff (first partitions) (last partitions))) (last partitions))
+                     (first partitions)))))))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (join (find-pattern
+   (map extract-keyword
+        message cipher))))
