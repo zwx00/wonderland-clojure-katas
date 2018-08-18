438429b9226da17786be96b07ba1168248266d02
zwx00 <bblokar@gmail.com>
2018-08-18 20:12:23 +0200

    naive solution for wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,18 @@
 (ns wonderland-number.finder)
 
+(defn get-multiples [num] (map #(* num %) [2, 3, 4, 5, 6]))
+
+(defn check-num [num] (apply = (conj
+                                 (map (comp set str)
+                                       (get-multiples num))
+                                 (set
+                                   (str num)) )))
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [x 100000]
+    (if (check-num x)
+      x
+      (recur (inc x))
+    )
+  )
+)
+
