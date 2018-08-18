a7b7b26a3f9893fb33951f2c33128859b3b7da9e
zwx00 <bblokar@gmail.com>
2018-08-18 19:38:52 +0200

    hardcoded solution for the problem lol

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,23 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [])
+
+(def the-rest [[[:fox :goose :corn :you] [:boat] []]
+               [[:fox :corn] [:boat :you :goose] []]
+               [[:fox :corn] [:boat] [:you :goose]]
+               [[:fox :corn] [:boat :you] [:goose]]
+               [[:fox :corn :you] [:boat] [:goose]]
+               [[:fox] [:boat :you :corn] [:goose]]
+               [[:fox] [:boat] [:you :goose :corn]]
+               [[:fox] [:boat :you :goose] [:corn]]
+               [[:fox :you :goose] [:boat] [:corn]]
+               [[:goose] [:boat :you :fox] [:corn]]
+               [[:goose] [:boat] [:corn :you :fox]]
+               [[:goose] [:boat :you] [:corn :fox]]
+               [[:goose :you] [:boat] [:corn :fox]]
+               [[] [:boat :you :goose] [:corn :fox]]
+               [[] [:boat] [:corn :fox :you :goose]]
+               ])
 
 (defn river-crossing-plan []
-  start-pos)
+  the-rest)
